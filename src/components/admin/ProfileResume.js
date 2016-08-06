b4734d89d642c9a415f68d9fb44a69c4224f930a
周勇 <wb-zy198077@alibaba-inc.com>
2016-08-06 14:17:17 +0800

    fix defect.

--- src/components/admin/ProfileResume.js
+++ src/components/admin/ProfileResume.js
@@ -41,7 +41,7 @@ class ProfileResume extends React.Component{
         let me = this;
         return (
             <div className="drip-ui-profile-resume">
-                <div className="tips">
+                <div className="tips-info">
                     <span>Tips:以下信息将在你博客中的&quot;关于我&quot;页面中展示.</span>
                     <button className="btn-1" onClick={me.handleSubmit.bind(me)}>保存</button>
                 </div>
